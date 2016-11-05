c9426b0e15dec05cef38cfd3fec6df877375859e
Eman Abbas <eman88abbas@gmail.com>
2016-11-05 16:10:25 -0700

    remove env

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\EMAN\Documents\Visual Studio 2013\Projects\Tamkeen4st\DjangoAzure\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
